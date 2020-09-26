1544768fce5a7f3f78e4eba1812cf4327a71df0a
Serge <sanichace@gmail.com>
2020-09-26 20:04:02 +0300

    added db migration and transaction controller

--- CRUDinAspCore/Startup.cs
+++ CRUDinAspCore/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 using CRUDinAspCore.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,7 +27,10 @@ namespace CRUDinAspCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddDbContext<TransactionDbContext>();
+            services.AddDbContext<TransactionDbContext>(options =>
+            
+                options.UseSqlServer(Configuration.GetConnectionString("DevConnection"))
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
